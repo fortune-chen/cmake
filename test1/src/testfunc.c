d5eff35b8cac7ae84552fa51a5733df938cd7b98
fortune.chen <fortune.chen@ubtrobot.com>
2022-05-11 13:48:01 +0800

    cmake file

---
+++ test1/src/testfunc.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <testfunc.h>
+
+void func(int data)
+{
+    printf("data is %d\n", data);
+}
---
+++ test1/src/testfunc1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <testfunc1.h>
+
+void func1(int data)
+{
+    printf("data is %d\n", data);
+}
